6d5a5f9ba43ae4be14fd9f837e66498e803149d1
Diego Pino <pinowsky@gmail.com>
2010-09-05 17:16:32 +0200

    Encode GET parameters in request URL

--- src/renfe/trains/services/RenfeXHR.java
+++ src/renfe/trains/services/RenfeXHR.java
@@ -3,8 +3,10 @@ package renfe.trains.services;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.Map;
 import java.util.Set;
 
@@ -40,25 +42,11 @@ public class RenfeXHR {
         return response;
     }
 
-    private String joinParameters(Map<String, String> params) {
-        String result = "";
-
-        Set<String> keys = params.keySet();
-        for (String each : keys) {
-            result += each + "=" + params.get(each) + "&";
-        }
-        if (result.length() > 0) {
-            result = "?" + result.substring(0, result.length() - 1);
-        }
-        return result;
-    }
-
     public void execute(Map<String, String> params) {
         try {
             String address = BASE_URL + joinParameters(params);
             URL url = new URL(address);
             response = url.openStream();
-            // Log.d(TAG, getResponseAsString());
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -66,7 +54,33 @@ public class RenfeXHR {
         }
     }
 
+    private String joinParameters(Map<String, String> params) {
+        String result = "";
+
+        Set<String> keys = params.keySet();
+        for (String each : keys) {
+            result += each + "=" + encode(params.get(each)) + "&";
+        }
+        if (result.length() > 0) {
+            result = "?" + result.substring(0, result.length() - 1);
+        }
+        return result;
+    }
+
+    private String encode(String str) {
+        try {
+            return URLEncoder.encode(str, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+
     private String toString(InputStream inputStream) {
+        if (inputStream == null) {
+            return "";
+        }
+
         StringWriter writer = new StringWriter();
         try {
             IOUtils.copy(inputStream, writer);
